fb6ac8082d8ad88fa0ec589e2fdab12e643c529b
tpersp <130591916+tpersp@users.noreply.github.com>
2025-03-30 22:07:25 +0200

    back to dev, 2.4.7

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ load_env()
 # Application Version & Paths
 # ------------------------------------------------------------
 
-APP_VERSION = "2.4.6" # Added Spotify progress bar.
+APP_VERSION = "2.4.7" # 
 
 VIEWER_HOME = os.environ.get("VIEWER_HOME", "/home/pi/PiViewer")
 IMAGE_DIR   = os.environ.get("IMAGE_DIR", "/mnt/PiViewers")
@@ -39,4 +39,4 @@ WEB_BG      = os.path.join(VIEWER_HOME, "web_bg.jpg")
 # ------------------------------------------------------------
 # Git Update Branch
 # ------------------------------------------------------------
-UPDATE_BRANCH = os.environ.get("UPDATE_BRANCH", "main")
+UPDATE_BRANCH = os.environ.get("UPDATE_BRANCH", "dev")
